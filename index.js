0beb4096b6febb8626dea7e67b5b58db974882a3
Mostafa Mahmoud Fouad <mostafa@heurolabs.com>
2023-10-09 04:24:02 +0300

    finished the project

---
+++ index.js
@@ -0,0 +1,34 @@
+import  express  from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = 'https://api.weatherapi.com/v1/';
+const KEY = '22470c34cea14d7aae075142233006';
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+let data;
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs',{content:data});
+});
+
+app.get('/search', async (req,res)=>{
+    const country = req.query.country;
+    console.log(country);
+    console.log(`${API_URL}current.json?key=${KEY}&q=${country}`)
+
+    try{
+        console.log(`${API_URL}current.json?key=${KEY}&q=${country}`)
+        const response = await axios.get(`${API_URL}current.json?key=${KEY}&q=${country}`);
+        data=response.data;
+        res.redirect('/');
+    }catch (er){
+        console.log(`error happend ${er.message}`);
+    }
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
